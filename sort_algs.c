10714e40f8234180b3f4942ee98817fcb4c021ba
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2024-06-03 16:07:06 +0300

    Update sort_algs.c

--- sort_algs.c
+++ sort_algs.c
@@ -1,6 +1,3 @@
-//
-// Created by ASUS on 31.05.2024.
-//
 
 #include <stdbool.h>
 
@@ -34,16 +31,16 @@ void _merge(int arr[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
+
     int L[n1], R[n2];
 
-    // Copy data to temp arrays L[] and R[]
+
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r
+
     i = 0;
     j = 0;
     k = l;
@@ -59,16 +56,14 @@ void _merge(int arr[], int l, int m, int r)
         k++;
     }
 
-    // Copy the remaining elements of L[],
-    // if there are any
+
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of R[],
-    // if there are any
+
     while (j < n2) {
         arr[k] = R[j];
         j++;
